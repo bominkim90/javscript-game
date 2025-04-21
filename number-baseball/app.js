eb79de9e0db654e7310508ba7d196961e05188b7
bominkim90 <bomin97@naver.com>
2025-04-21 22:18:23 +0900

    단계2 : 게임 기록 저장 및 조회 기능 추가 => 모듈화는 점차점차.. 모든 기능 다 만들고 한꺼번에 정리하려합니다!

--- number-baseball/app.js
+++ number-baseball/app.js
@@ -1,123 +1,171 @@
-// 기본 한 줄 입력받기
-// inputInterface.on("line", (line) => {
-//   // 한 줄씩 입력받은 후 실행할 코드
-//   console.log(line); // 입력된 값은 line에 저장된다.
-//   inputInterface.close(); // 필수!! close가 없으면 입력을 무한히 받는다.
-//   // inputInterface.close()가 호출되면 inputInterface.on("close", 콜백함수) 가 실행됨
-// });
-
-// inputInterface.on('close', () => {
-//   // 입력이 끝난 후 실행할 코드
-//   console.log("프로그램 종료")
-//   process.exit();
-// })
-
-
     
+/* 초기값 세팅 */
+
 // 콘솔에서 값 받을 수 있게 해주는 모듈 불러오기
 const readline = require('readline');
+// 모듈 실행 => 콘솔에서 값을 입력 받아서 서버내에서 처리할 수 있게 해줌
 const inputInterface = readline.createInterface({ 
   input: process.stdin, // 표준 입력 스트림
   output: process.stdout // 표준 출력 스트림
 });
-// 콘솔에 입력값을 받아서 내 서버에서 값을 처리할 수 있게 해줌
-// 이때부터 사용자 콘솔 입력을 기다림
 
-
-// { 1 입력 => 게임 시작 + 3자리 랜덤숫자 생성 / 9 입력 => app 완전 종료 }
 console.log("게임을 새로 시작하려면 1, 종료하려면 9를 입력하세요.");
 let computerNumbers = [];
-let nowPlaying = false; // 숫자 1은 게임 진행중엔 못받게 하기 위해
+let nowPlaying = false; // (게임중 판별 위해) 숫자 1은 게임 진행중엔 못받게 하기 위해
+let totalRecords = [];
+let oneGameRecord = {
+  totalTry : 0,
+  startTime : "",
+  endTime : "",
+  "게임내역" : []
+};
+let viewingRecord = false; // (기록 조회중 판별 위해) 기록 조회중엔 기록조회를 위한 입력값 처리
+
+/*// 초기값 세팅 */
+
+
 
 
 // 콘솔 입력값 받기
-inputInterface.on("line", (line) => {
-  if(line == 9) {
-    return endGame();
+inputInterface.on("line", (number) => {
+  if(viewingRecord){
+    if(number.length == 1){
+      if(number == 0){ // 기록 종료
+        viewingRecord = false;
+        console.log("------- 기록 종료 -------\n");
+        console.log("게임을 새로 시작하려면 1, 기록을 보려면 2, 종료하려면 9를 입력해주세요.");
+        return;
+      }
+      if(totalRecords[parseInt(number)-1]){
+        // 게임 기록 조회
+        console.log(totalRecords[parseInt(number)-1]);
+        console.log("확인할 게임 번호를 입력하세요 (종료하려면 0을 입력)");
+      }
+      else {
+        console.log("확인할 게임 번호를 정확히 입력하세요");
+      }
+    }
+    else {
+      console.log("확인할 게임 번호를 정확히 입력하세요");
+    }
+    return;
+  }
+  if(number == 9) {
+    return exitGame();
+  }
+  if(number == 2) {
+    viewingRecord = true;
+    return viewRecord();
   }
 
-  if(nowPlaying){ // 게임 진행중이라면 => 3자리 숫자만 받아서 hint 제공함
-    if(line && parseInt(line) > 0 && line.length === 3) {
-      return giveHint(line);
-    } else {
-      console.log("세자리 숫자를 입력해주세요");
+  if(nowPlaying){ // 게임중 O => (게임 진행중이냐 or 한판 종료된 시점이냐)
+    if(oneGameRecord.endTime){ // 이건 한판 종료된 시점 && 재시작하겠다 하면
+      if(number == 1) restartGame();
+      else console.log("게임을 새로 시작하려면 1, 기록을 보려면 2, 종료하려면 9를 입력하세요.");
+    } 
+    else { // 게임 계속 진행중
+      if(number && parseInt(number) > 0 && number.length === 3) {
+        return checkNumber(number);
+      } 
+      else {
+        console.log("세자리 숫자를 입력해주세요");
+      }
     }
-  } else { // 게임중이 아닐때만 => 숫자1을 받을 수 있음
-    if(line == 1) {
+  } 
+  else { // 게임중 X => 숫자1만 받을 수 있도록 체크
+    if(number == 1) {
       return startGame();
-    } else {
+    } 
+    else {
       console.log("숫자 1을 눌러 게임을 시작해주세요")
     }
   }
 });
 
 
+
 /************************************************************************/
-// 기능별 함수
+
+// 프로그램 완전 종료
+function exitGame(){
+  console.clear();
+  console.log("프로그램을 종료하였습니다.")
+  process.exit();
+}
 
 
-
+// 게임 다시 시작 시 => 컴퓨터숫자, oneGameRecord 초기화
+function restartGame(){
+  oneGameRecord = { // oneGame 데이터 초기화
+    totalTry : 0,
+    startTime : "",
+    endTime : "",
+    "게임내역" : []
+  };
+  
+  startGame();
+}
 
 // 게임 시작 (= 중복되지 않는 랜덤숫자 3개 뽑기)
 function startGame(){
-  computerNumbers = [];
+  computerNumbers = []; // 랜덤숫자 초기화
+  oneGameRecord.startTime = new Date();
+
   console.log("컴퓨터가 숫자를 뽑았습니다.");
-  
-  while(computerNumbers.length < 3){ // 3 length 되면 종료
+  while(computerNumbers.length < 3){
     let randomNumber = Math.floor(Math.random()*10);
-    if( randomNumber !== 0 && randomNumber !== 10 && !computerNumbers.includes(randomNumber)) {
+    if( randomNumber !== 0 && randomNumber !== 10 && !computerNumbers.includes(''+randomNumber+'')) {
       computerNumbers.push(''+randomNumber+'');
     }
   }
-
   console.log('생성된 3자리 숫자는 이렇습니다.', computerNumbers);
-  computerNumbers.forEach( (value, index) => console.log(value) ); // 이건 테스터 확인용
-
+  computerNumbers.forEach( (value, index) => console.log(value) );
   console.log("숫자를 입력해주세요.");
   nowPlaying = true;
 }
 
 
-// 게임 종료
-function endGame(){
-  console.log("프로그램을 종료하였습니다.")
-  nowPlaying = false;
-  process.exit();
-}
+// (게임시작 후) 사용자숫자 검사
+function checkNumber(line){
+  oneGameRecord.totalTry++;
 
-
-/*
-  힌트 제공
-- 스트라이크 : 입력한 숫자가 컴퓨터의 숫자와 *위치까지 동일*한 경우
-- 볼 : 입력한 숫자가 컴퓨터의 숫자에 포함되지만 *위치가 다를 경우*
-- 낫싱 : 입력한 숫자가 *컴퓨터의 숫자에 전혀 포함되지 않을 경우*
-
-  *** computerNumbers객체, humanNumber객체의 이중반복문을 돌린다 ***
-  * strike랑 ball 추가 방법
-  예) <computerNumbers 객체 '첫번째' 값> 기준으로 <humanNumber 객체> 반복문이 돌아갈때
-  => value값이 같은경우
-    서로의 index를 비교 => 같으면 strike++ / 다르면 ball++
-*/
-function giveHint(line){
-  let humanNumbers = line.split(''); // 사람이 넣는 숫자는 중복을 허용해야 하기에(111 이런식) => Array 자료형 
-  // (key,value)를 값으로 가지는 자료형은 key값에 원시데이터를 넣을경우, 중복되는 key값을 가질수 없게한다
+  let humanNumbers = line.split('');
   console.log("humanNumbers : ", humanNumbers);
   let [strike, ball] = [0, 0];
-
   console.log("computerNumbers : ",computerNumbers);
-
   computerNumbers.forEach( (computerEachNumber, computerEachIndex) => {
-
     humanNumbers.forEach ( (humanEachNumber, humanEachIndex) => {
       if(computerEachNumber == humanEachNumber){
         if(computerEachIndex == humanEachIndex) {return strike++}
         else {return ball++}
       }
     });
-
   });
   console.log(`${strike} 스트라이크, ${ball} 볼 \n숫자를 입력하세요.`);
+  oneGameRecord["게임내역"].push(`플레이어가 입력한 숫자 : ${line}, 컴퓨터 응답 : ${strike} 스트라이크, ${ball} 볼`);
+  
+  if(strike >= 3) {
+    saveRecord(oneGameRecord); // 게임 한판 종료 => 기록저장
+  }
+}
 
-  // strike가 3이 되면 => 게임 종료
-  if(strike >= 3) endGame()
+
+// 게임 기록 저장
+// 1.게임시작시간 2.게임종료시간 3.총시도횟수 4.게임진행내역(사용자 입력 숫자와 컴퓨터의 피드백 내용)
+function saveRecord(oneGameRecord){
+  oneGameRecord.endTime = new Date();
+  totalRecords.push(oneGameRecord);
+  computerNumbers = [];
+  console.log(oneGameRecord);
+  console.log("게임을 새로 시작하려면 1, 기록을 보려면 2, 종료하려면 9를 입력하세요.");
+  // 새로운 게임 시작하기
+  // 모든걸 다 리셋이 아니라,,
+  // 리셋해야할 것 =>  컴퓨터숫자, oneGameRecord
+}
+
+
+// 게임 기록 조회
+function viewRecord(){
+  console.log(totalRecords);
+  console.log("확인할 게임 번호를 입력하세요 (종료하려면 0을 입력)");
 }
